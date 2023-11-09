91d02246e8c0319ddc0fdc63ed98fa71318cf401
thompson-jacob <63691359+thompson-jacob@users.noreply.github.com>
2023-11-08 22:01:50 -0700

    OOP quiz, start tic-tac-toe psuedo code logic

---
+++ ruby_projects/OOP/objects.rb
@@ -0,0 +1,44 @@
+
+# module Speak 
+#   def speak(sound)
+#     puts "#{sound}"
+#   end
+# end
+
+# class Creature
+#   def initialize(name, species)
+#     @name = name
+#     @species = species
+#   end
+#   include Speak
+
+#   def get_name
+#     @name
+#   end 
+
+#   def set_name(name)
+#     @name = name
+#   end
+
+#   def get_species
+#     @species
+#   end
+
+#   def set_species (species)
+#     @species = species
+#   end
+# end
+
+
+
+# # puts Creature.ancestors
+
+
+# human = Creature.new("Jim", "human")
+# # human.speak("hello")
+# human.set_name="Bob"
+
+# puts "hi".methods
+# puts String.new("hi").methods
+
+puts [].methods
